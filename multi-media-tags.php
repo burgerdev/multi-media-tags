25b9c1bb2d6f859d3c8dcad57385084a903f8c4a
burgerdev <webmaster@burgerdev.de>
2013-01-31 23:23:05 +0100

    added shortcode handling and image linkification

--- multi-media-tags.php
+++ multi-media-tags.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Multi Media Tags
-Plugin URI: https://github.com/burgerdev/multi-media-tags
+Plugin URI: http://burgerdev.de/multi-media-tags/
 Description: advanced handling of media types for the media tags plugin
 Version: 0.1
 Author: Markus Döring
@@ -28,7 +28,59 @@ License: GPL3
  */
 
 
-function bdev_mt_item_callback($post_item, $size='SEP')
+/*******************************************************/
+/******************* ERROR HANDLING ********************/
+
+function bdev_mmt_check(){
+	
+	if ( ! function_exists('mediatags_shortcode_handler') ) {
+		add_action('admin_notices', 'bdev_mmt_admin_error_notice');
+		return false;
+	} else {
+		return true;
+	}
+}
+
+function bdev_mmt_admin_error_notice(){
+    echo '<div class="error">
+       <p>The plugin <strong>Media Tags</strong> must be installed and activated for using <em>Multi Media Tags</em></p>
+    </div>';
+}
+
+if ( ! function_exists('mediatags_shortcode_handler') ) {
+	bdev_mmt_error();
+}
+
+
+/*******************************************************/
+/***************** SHORTCODE HANDLING ******************/
+
+function bdev_mmt_shortcode_handler($atts, $content=null, $tableid=null){
+	$atts['display_item_callback'] = "bdev_mmt_item_callback";
+
+	if ( ! bdev_mmt_check() ) {
+		return "<strong>Internal Error</strong>";
+	else {
+		return mediatags_shortcode_handler($atts, $content, $tableid);
+	}
+
+}
+
+add_shortcode('multi-media-tags', 'bdev_mmt_shortcode_handler');
+
+/*******************************************************/
+/******************** ACTUAL CODE **********************/
+
+
+
+/* function to call for media tags shortcode handler
+ *
+ * @param post_item the attachment
+ * @param size not used ...
+ *
+ * @return attachment as <li> item
+ */
+function bdev_mmt_item_callback($post_item, $size='SEP')
 {
 	//echo "post_item<pre>"; print_r($post_item); echo "</pre>";
 	$image_src      = wp_get_attachment_image_src($post_item->ID, $size);
@@ -36,7 +88,7 @@ function bdev_mt_item_callback($post_item, $size='SEP')
 
 	$out =  '<li class="media-tag-list" id="media-tag-item-'.$post_item->ID.'">';
 
-	$out .= bdev_mt_handle_mime($post_item);
+	$out .= bdev_mmt_handle_mime($post_item);
 
 	$out .= '</li>';
 
@@ -44,13 +96,20 @@ function bdev_mt_item_callback($post_item, $size='SEP')
 
 }
 
-function bdev_mt_handle_mime($post) {
+
+/*
+ *
+ * @param post the attachment
+ * 
+ * @return custom html depending on attachment
+ */
+function bdev_mmt_handle_mime($post) {
 	$link = wp_get_attachment_url($post->ID);
 	$mime = $post->post_mime_type;
 
-	if (bdev_mt_is_image($mime)) {
+	if (bdev_mmt_is_image($mime)) {
 		$out = '<img src="'.$image_src[0].'" width="'.$image_src[1].'" height="'.$image_src[2].'" title="'.$post_item->post_title.'" />';
-	} else if (bdev_mt_is_audio($mime)) {
+	} else if (bdev_mmt_is_audio($mime)) {
 		$out = '<audio controls="controls" preload="none"><source src="'.$link.'" type="'.$mime.'" /></audio>';
 	} else {
 		// default handling of unknown media types
@@ -62,12 +121,12 @@ function bdev_mt_handle_mime($post) {
 
 }
 
-function bdev_mt_is_image($mime) {
+function bdev_mmt_is_image($mime) {
 	// we want to return real true / false, not 0 or 1
 	return preg_match("/image\//", $mime) ? true : false;
 }
 
-function bdev_mt_is_audio($mime) {
+function bdev_mmt_is_audio($mime) {
 	// we want to return real true / false, not 0 or 1
 	return preg_match("/audio\//", $mime) ? true : false;
 }
