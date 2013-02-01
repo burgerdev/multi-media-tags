cb407e1a6e56005c95f7cc61a7d9565fbfe41e5d
der-burger <der-burger@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-01 21:12:52 +0000

    Squashed commit of the following:
    commit aa563c0958c56dc3d87ca2accbb5a56691dcf08c
    Author: burgerdev <webmaster@burgerdev.de>
    Date:   Fri Feb 1 22:03:01 2013 +0100
    
        ready for svn check-in
    
    commit 7d00ca1bf50475526b63ad40aca0e54d755c2d6e
    Merge: 4c58b02 25b9c1b
    Author: burgerdev <webmaster@burgerdev.de>
    Date:   Fri Feb 1 22:00:34 2013 +0100
    
        Merge branch 'master' of github.com:burgerdev/multi-media-tags
    
    commit 25b9c1bb2d6f859d3c8dcad57385084a903f8c4a
    Author: burgerdev <webmaster@burgerdev.de>
    Date:   Thu Jan 31 23:23:05 2013 +0100
    
        added shortcode handling and image linkification
    
    commit 6ab2dbfb2d34f4d89ea6c2f25eb7f0bc05cc87ca
    Author: Markus Döring <webmaster@burgerdev.de>
    Date:   Sat Jan 26 01:03:45 2013 +0100
    
        tested with wp readme validator
    
    commit f21ac48edf8ecafba6cdcb162bedbe0022f99784
    Author: Markus Döring <webmaster@burgerdev.de>
    Date:   Sat Jan 26 00:40:47 2013 +0100
    
        wordpress.org conforming readme
    
    commit 585ef48262e6f4e1a5f74387c9b2e0e87d52344d
    Author: Markus Döring <markus.b.doering@gmail.com>
    Date:   Fri Jan 25 14:38:48 2013 +0100
    
        extensive licencing information
    
    commit 5bf6c99d539f41a02ca7d3789e87592b35536b1b
    Author: Markus Döring <markus.b.doering@gmail.com>
    Date:   Fri Jan 25 14:32:34 2013 +0100
    
        made fit for WP hosting
    
    commit 2ca57aa4830e6b247fc2a01b28e373aa60b03a38
    Author: Markus Döring <markus.b.doering@gmail.com>
    Date:   Fri Jan 25 14:25:24 2013 +0100
    
        adapted for name change
    
    commit c4a0d780e82b8903f926367530bbab749e4f4289
    Author: Markus Döring <webmaster@burgerdev.de>
    Date:   Fri Jan 25 14:13:08 2013 +0100
    
        more documentation
    
    commit 7411d94fba94dbbe05e2f18cdfdff526a77baf13
    Author: Markus Döring <webmaster@burgerdev.de>
    Date:   Fri Jan 25 14:00:16 2013 +0100
    
        image, audio and fallback implemented
    
    commit dacf7ec182a5bf661cfb98184be5d0a92f83f34d
    Author: Markus Döring <webmaster@burgerdev.de>
    Date:   Fri Jan 25 04:48:26 2013 -0800
    
        Initial commit
    
    git-svn-id: http://plugins.svn.wordpress.org/multi-media-tags/trunk@662316 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ multi-media-tags.php
@@ -0,0 +1,135 @@
+<?php
+/*
+Plugin Name: Multi Media Tags
+Plugin URI: http://burgerdev.de/multi-media-tags/
+Description: advanced handling of media types for the media tags plugin
+Version: 0.1
+Author: Markus Döring
+Author URI: http://burgerdev.de
+License: GPLv3
+*/
+
+
+/*
+    Copyright (C) 2013 Markus Döring
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/gpl.txt.
+ */
+
+
+/*******************************************************/
+/******************* ERROR HANDLING ********************/
+
+function bdev_mmt_check(){
+	
+	if ( ! function_exists('mediatags_shortcode_handler') ) {
+		add_action('admin_notices', 'bdev_mmt_admin_error_notice');
+		return false;
+	} else {
+		return true;
+	}
+}
+
+function bdev_mmt_admin_error_notice(){
+    echo '<div class="error">
+       <p>The plugin <strong>Media Tags</strong> must be installed and activated for using <em>Multi Media Tags</em></p>
+    </div>';
+}
+
+if ( ! function_exists('mediatags_shortcode_handler') ) {
+	bdev_mmt_error();
+}
+
+
+/*******************************************************/
+/***************** SHORTCODE HANDLING ******************/
+
+function bdev_mmt_shortcode_handler($atts, $content=null, $tableid=null){
+	$atts['display_item_callback'] = "bdev_mmt_item_callback";
+
+	if ( ! bdev_mmt_check() ) {
+		return "<strong>Internal Error</strong>";
+	else {
+		return mediatags_shortcode_handler($atts, $content, $tableid);
+	}
+
+}
+
+add_shortcode('multi-media-tags', 'bdev_mmt_shortcode_handler');
+
+/*******************************************************/
+/******************** ACTUAL CODE **********************/
+
+
+
+/* function to call for media tags shortcode handler
+ *
+ * @param post_item the attachment
+ * @param size not used ...
+ *
+ * @return attachment as <li> item
+ */
+function bdev_mmt_item_callback($post_item, $size='SEP')
+{
+	//echo "post_item<pre>"; print_r($post_item); echo "</pre>";
+	$image_src      = wp_get_attachment_image_src($post_item->ID, $size);
+
+
+	$out =  '<li class="media-tag-list" id="media-tag-item-'.$post_item->ID.'">';
+
+	$out .= bdev_mmt_handle_mime($post_item);
+
+	$out .= '</li>';
+
+	return $out;
+
+}
+
+
+/*
+ *
+ * @param post the attachment
+ * 
+ * @return custom html depending on attachment
+ */
+function bdev_mmt_handle_mime($post) {
+	$link = wp_get_attachment_url($post->ID);
+	$mime = $post->post_mime_type;
+
+	if (bdev_mmt_is_image($mime)) {
+		$out = '<img src="'.$image_src[0].'" width="'.$image_src[1].'" height="'.$image_src[2].'" title="'.$post_item->post_title.'" />';
+	} else if (bdev_mmt_is_audio($mime)) {
+		$out = '<audio controls="controls" preload="none"><source src="'.$link.'" type="'.$mime.'" /></audio>';
+	} else {
+		// default handling of unknown media types
+		//TODO does wp_get_attachment_url fail sometimes? catch it!
+		$out =  '<a href="'.$link.'">'.$post->post_title.'</a>';
+	}
+
+	return $out;
+
+}
+
+function bdev_mmt_is_image($mime) {
+	// we want to return real true / false, not 0 or 1
+	return preg_match("/image\//", $mime) ? true : false;
+}
+
+function bdev_mmt_is_audio($mime) {
+	// we want to return real true / false, not 0 or 1
+	return preg_match("/audio\//", $mime) ? true : false;
+}
+
+
+?>
